94d5d2a1e7b016bb1eb8030ad23bcc49eb8ba3d9
rowlindsay <rowlindsay@gmail.com>
2016-10-01 10:15:26 -0700

    added activities to buttons

--- app/src/main/java/com/hackersoflg/genderbender/HomeScreen.java
+++ app/src/main/java/com/hackersoflg/genderbender/HomeScreen.java
@@ -1,11 +1,13 @@
 package com.hackersoflg.genderbender;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
 
 public class HomeScreen extends AppCompatActivity {
 
@@ -26,5 +28,33 @@ public class HomeScreen extends AppCompatActivity {
             }
         });
         */
+
+        Button addEmployeeButton = (Button) findViewById(R.id.employeebutton);
+
+        addEmployeeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startNewEmployeeActivity();
+            }
+        });
+
+        Button keyButton = (Button) findViewById(R.id.keybutton);
+
+        keyButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startKeyActivity();
+            }
+        });
+    }
+
+    public void startNewEmployeeActivity() {
+        Intent intent = new Intent(this, employee_add.class);
+        startActivity(intent);
+    }
+
+    public void startKeyActivity() {
+        Intent intent = new Intent(this, key.class);
+        startActivity(intent);
     }
 }
