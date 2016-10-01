949f0d46d0e33242ee383c22ac012e143b17c907
rowlindsay <rowlindsay@gmail.com>
2016-10-01 11:06:07 -0700

    added choices to spinners

--- app/src/main/java/com/hackersoflg/genderbender/employee_add.java
+++ app/src/main/java/com/hackersoflg/genderbender/employee_add.java
@@ -2,6 +2,8 @@ package com.hackersoflg.genderbender;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class employee_add extends AppCompatActivity {
 
@@ -9,5 +11,17 @@ public class employee_add extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_employee_add);
+
+        Spinner companyPicker = (Spinner) findViewById(R.id.spinner);
+        ArrayAdapter<CharSequence> adapter1 = ArrayAdapter.createFromResource(this,
+                R.array.company_choices, android.R.layout.simple_spinner_item);
+        adapter1.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        companyPicker.setAdapter(adapter1);
+
+        Spinner genderPicker = (Spinner) findViewById(R.id.spinner2);
+        ArrayAdapter<CharSequence> adapter2 = ArrayAdapter.createFromResource(this,
+                R.array.gender_choices, android.R.layout.simple_spinner_item);
+        adapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        genderPicker.setAdapter(adapter2);
     }
 }
